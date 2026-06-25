da04fef0f65605493407ad63a4fe1456d8bc69a8
Guru6126 <gurukarthikeyan2k06@gmail.com>
2026-06-25 11:48:23 +0530

    Initial project setup

---
+++ src/main/java/com/gurukarthikeyan/interviewschedulermanagement/InterviewschedulermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.gurukarthikeyan.interviewschedulermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewschedulermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewschedulermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gurukarthikeyan/interviewschedulermanagement/InterviewschedulermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurukarthikeyan.interviewschedulermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewschedulermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
