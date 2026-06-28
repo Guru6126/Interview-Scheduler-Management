048d21a63f54ada0d47ea8aec18ba70d5f24232b
Guru6126 <gurukarthikeyan2k06@gmail.com>
2026-06-28 19:00:33 +0530

    Created Availability Entity

---
+++ src/main/java/com/gurukarthikeyan/interviewschedulermanagement/model/Availability.java
@@ -0,0 +1,35 @@
+package com.gurukarthikeyan.interviewschedulermanagement.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "availability")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Availability {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate availableDate;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @Column(nullable = false)
+    private Boolean available;
+}
