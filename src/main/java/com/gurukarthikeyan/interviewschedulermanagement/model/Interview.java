d398ecee0e49968c97b19f98e201bb4db992e1bc
Guru6126 <gurukarthikeyan2k06@gmail.com>
2026-06-28 18:48:59 +0530

    Created Interview Entity

---
+++ src/main/java/com/gurukarthikeyan/interviewschedulermanagement/model/Interview.java
@@ -0,0 +1,39 @@
+package com.gurukarthikeyan.interviewschedulermanagement.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "interviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Interview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate interviewDate;
+
+    @Column(nullable = false)
+    private LocalTime interviewTime;
+
+    @Column(nullable = false)
+    private String interviewMode;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(length = 1000)
+    private String feedback;
+}
