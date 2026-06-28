26771e91b808d41b3154297fd16522530b80558b
Guru6126 <gurukarthikeyan2k06@gmail.com>
2026-06-28 18:52:46 +0530

    Created Feedback Entity

---
+++ src/main/java/com/gurukarthikeyan/interviewschedulermanagement/model/Feedback.java
@@ -0,0 +1,29 @@
+package com.gurukarthikeyan.interviewschedulermanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "feedback")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String rating;
+
+    @Column(length = 1000)
+    private String comments;
+
+    @Column(nullable = false)
+    private String recommendation;
+}
